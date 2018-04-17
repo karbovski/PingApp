fe1a32de3e0930e2255770b306089909c120b478
Dominik Karbowski <karbowski.dominik@gmail.com>
2018-04-17 13:57:40 +0200

    update

--- app/src/main/java/no/karbowski/pingapp/LogActivity.java
+++ app/src/main/java/no/karbowski/pingapp/LogActivity.java
@@ -2,12 +2,53 @@ package no.karbowski.pingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class LogActivity extends AppCompatActivity {
+
+    LinearLayout layout;
+    Scanner scanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        layout = findViewById(R.id.logLayout);
+        readLogData();
+    }
+
+    private void readLogData(){
+
+        try {
+            scanner = new Scanner(openFileInput("log.txt"));
+
+            while (scanner.hasNextLine()){
+                TextView newLogLine = new TextView(this);
+                newLogLine.setText(scanner.nextLine());
+                layout.addView(newLogLine);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            scanner.close();
+        }
+    }
+
+    public void clearLogOnClick(View view){
+        try {
+            layout.removeAllViews();
+            PrintStream output = new PrintStream(openFileOutput("log.txt", MODE_PRIVATE));
+            output.print("");
+            output.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/no/karbowski/pingapp/MainActivity.java
+++ app/src/main/java/no/karbowski/pingapp/MainActivity.java
@@ -1,27 +1,30 @@
 package no.karbowski.pingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText hostnameEditText;
+    TextView latencyTextView;
+    String hostname;
+    PrintStream output;
+
     public void pingOnClick(View view) {
-
-        EditText hostnameEditText = findViewById(R.id.hostNameEditText);
-
-        TextView latencyTextView = findViewById(R.id.latencyTextView);
-
-        String hostname = hostnameEditText.getText().toString();
-
         try {
-
+            hostname = hostnameEditText.getText().toString();
             double latency = Ping.ping(hostname);
             String latencyString = "Latency: " + latency + " ms";
             latencyTextView.setText(latencyString);
+            output.println(hostname + " " + latency + "ms");
 
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -32,5 +35,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hostnameEditText = findViewById(R.id.hostNameEditText);
+
+        latencyTextView = findViewById(R.id.latencyTextView);
+
+        try {
+            output = new PrintStream(openFileOutput("log.txt",MODE_APPEND | MODE_PRIVATE));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void showLogClick(View view){
+        Intent intent = new Intent(this, LogActivity.class);
+        startActivity(intent);
     }
 }
